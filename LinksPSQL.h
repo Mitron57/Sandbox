f3a5e4046257ca070feced33fb39838805c4d7d9
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2022-03-13 20:58:11 +0400

    hope it`s ok

--- LinksPSQL.h
+++ LinksPSQL.h
@@ -5,39 +5,29 @@ namespace Platform::Data::Doublets
     template<typename TLink>
     struct LinksPSQL: public ILinks<LinksPSQL<TLink>, TLink>
     {
-        auto foo() -> std::string //test
-        {
-            return c.dbname();
-        }
-        
-        explicit LinksPSQL(const std::string& dbopts) : opts(dbopts)
+        explicit LinksPSQL(const std::string& dbopts) : opts(std::move(dbopts))
         {
             query = "CREATE TABLE IF NOT EXISTS Links(id bigint, from_id bigint, to_id bigint);";
-            w.exec(query);
+            line.insert(query);
         }
         
-        virtual ~LinksPSQL()
+        ~LinksPSQL()
         {
-            w.commit();
-            c.close();
+            line.complete();
+            trans.commit();
+            conn.close();
         }
         
         auto Exists(auto&& Index) -> bool
         {
-            pqxx::result r = w.exec("SELECT * FROM Links WHERE id = " + std::to_string(Index) + ";");
-            if(r[0][0].c_str()=="")
-                return false;
-            else
-                return true;
+            line.insert("SELECT * FROM Links WHERE id = " + std::to_string(Index) + ";");
+            auto result = line.retrieve().second;
+            return result[0][0].c_str() != "";
         }
         
         auto Create(Interfaces::CArray auto&& substitution) -> void
         {
-            query = "SELECT * FROM Links;";
-            pqxx::result r = w.exec(query);
-            query = "INSERT INTO Links VALUES (" + std::to_string(1) + ", " 
-            + std::to_string(substitution[0]) + ", " + std::to_string(substitution[1]) + ")";
-            w.exec(query);
+            line.insert("INSERT INTO Links VALUES (" + std::to_string(++index) + ", " + std::to_string(substitution[0]) + ", " + std::to_string(substitution[1]) + ");");
         }
         
         auto Update(Interfaces::CArray auto&& restrictions, Interfaces::CArray auto&& substitution) -> void
@@ -53,7 +43,7 @@ namespace Platform::Data::Doublets
                 + std::to_string(substitution[1]) + " WHERE from_id = " + std::to_string(restrictions[0])
                 + "AND to_id = " + std::to_string(restrictions[1]) + ";";
             }
-            w.exec(query);
+            line.insert(query);
         }
         
         auto Delete(Interfaces::CArray auto&& restrictions) -> void
@@ -63,7 +53,7 @@ namespace Platform::Data::Doublets
             else
             {
                 query = "DELETE FROM Links WHERE id = " + std::to_string(restrictions[0]) + ";";
-                w.exec(query);
+                line.insert(query);
             }
         }
         
@@ -72,16 +62,17 @@ namespace Platform::Data::Doublets
             LinksConstants<TLink> constants;
             if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
                 query = "SELECT COUNT(*) FROM Links;";
-            if (restrictions[0] != constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
+            else if (restrictions[0] != constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
                 query = "SELECT COUNT(*) FROM Links WHERE id = " + std::to_string(restrictions[0]) + ";";
-            if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] == constants.Any)
+            else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] == constants.Any)
                 query = "SELECT COUNT(*) FROM Links WHERE from_id = " + std::to_string(restrictions[1]) + ";";
-            if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] != constants.Any)
+            else if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] != constants.Any)
                 query = "SELECT COUNT(*) FROM Links WHERE to_id = " + std::to_string(restrictions[2]) + ";";
-            if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] != constants.Any)
+            else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] != constants.Any)
                 query = "SELECT COUNT(*) FROM Links WHERE from_id = " + std::to_string(restrictions[1]) + "AND to_id = " + std::to_string(restrictions[2]) + ";";
-            pqxx::result r = w.exec(query);
-            return r[0][0].as<int>();
+            line.insert(query);
+            auto result = line.retrieve().second;
+            return result[0][0].as<int>();
         }
         
         auto Each(Interfaces::CArray auto&& restrictions) -> void /*requires requires {std::integral<TLink>;}*/
@@ -89,31 +80,41 @@ namespace Platform::Data::Doublets
             LinksConstants<TLink> constants;
             if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
                 query = "SELECT * FROM Links;";
-            if (restrictions[0] != constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
+            else if (restrictions[0] != constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
                 query = "SELECT * FROM Links WHERE id = " + std::to_string(restrictions[0]) + ";";
-            if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] == constants.Any)
+            else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] == constants.Any)
                 query = "SELECT * FROM Links WHERE from_id = " + std::to_string(restrictions[1]) + ";";
-            if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] != constants.Any)
+            else if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] != constants.Any)
                 query = "SELECT * FROM Links WHERE to_id = " + std::to_string(restrictions[2]) + ";";
-            if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] != constants.Any)
+            else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] != constants.Any)
                 query = "SELECT * FROM Links WHERE from_id = " + std::to_string(restrictions[1]) + "AND to_id = " + std::to_string(restrictions[2]) + ";";
-            pqxx::result r = w.exec(query);
+            line.insert(query);
+            auto result = line.retrieve().second;
             Link<TLink> link (0, 0, 0);
-            for(int i{}; i<r.size(); i++)
+            for(int i{}; i<result.size(); i++)
             {
                 for(int j{}; j<3; j++)
                 {
-                    link[j] = r[i][j].as<TLink>();
+                    link[j] = result[i][j].as<TLink>();
                 }
                 std::cout<<link<<'\n';
             }
-            
         }
         
         private: std::string opts{};
-        private: std::string_view query{};
+        private: std::string query{};
+        private: int index{};
         
-        private: pqxx::connection c{opts};
-        private: pqxx::work w{c};
+        private: constexpr void GetIndex()
+        {
+            pqxx::work trans2{conn};
+            pqxx::result r = trans2.exec("SELECT * FROM Links;");
+            trans2.commit();
+            index = r.size();
+        }
+        
+        private: pqxx::connection conn{opts};
+        private: pqxx::work trans{conn};
+        private: pqxx::pipeline line{trans};
     };
 }
