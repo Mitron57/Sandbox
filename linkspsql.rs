45d5f4ef96b33927664b2a226521195727ec300a
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2022-04-19 19:59:32 +0400

    wow rust

--- linkspsql.rs
+++ linkspsql.rs
@@ -1,30 +1,27 @@
 pub extern crate tokio_postgres;
-pub use tokio_postgres::{*, Client, Connection, NoTls, Socket, tls::NoTlsStream};
+
+pub use tokio_postgres::{Client, NoTls, tls::NoTlsStream, Error, Statement};
 
 pub struct LinksPSQL {
     pub client: Client,
-    pub connection: Connection<Socket, NoTlsStream>
+    //pub connection: Connection<Socket, NoTlsStream> //maybe we don`t need this
 }
 impl LinksPSQL {
-    pub async fn new(dbopts: &str) -> Result<Self, Box<dyn std::error::Error>> {
-        let (client, connection) = tokio_postgres::connect(dbopts, NoTls).await?;
-        Ok(Self {
-            client,
-            connection,
-        })
+    pub fn new(client: Client) -> Result<Self, Error> {
+        //let connection = tokio_postgres::connect(dbopts, NoTls).await?.1;
+        Ok(Self { client })
     }
-}
-
-#[cfg(test)]
-mod test {
-    use super::LinksPSQL;
-
-    #[tokio::test]
-    async fn test() {
-        let opts = "your_db_params";
-        assert!(match LinksPSQL::new(opts).await {
-            Ok(_) => true,
-            Err(_) => false,
-        }); 
+    
+    pub async fn create(&self, substitution: &[i32]) -> Result<Statement, Error> {
+        self.client.prepare(&format!(
+            "INSERT INTO Links VALUES ({}, {}, {});", 
+            substitution[0], substitution[1], substitution[2])).await
+    }
+    
+    /*pub async fn count(&self, restriction: &[i32]) -> i32 {
+        let result = self.client.prepare(&format!("SELECT (*) COUNT FROM Links WHERE id = {}", restriction[0])).await?;
+    }*/
+    pub async fn delete(&self, restriction: &[i32]) -> Result<Statement, Error> {
+        self.client.prepare(&format!("DELETE FROM Links WHERE id = {}", restriction[0])).await
     }
 }
